90c9d55ada43108e0d419a5a1c161d78e5d571a0
RinchenTuladhar <rinchentul@gmail.com>
2020-07-13 19:25:40 +0100

    Seperated home and skills page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,12 @@
 import React from "react";
-import Panel from "./Panel";
+import Skills from "./Skills";
 
 export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <h1>Experienced with:</h1>
-        <div className="flex-time">
-          <Panel title="Ruby on Rails" />
-          <Panel title="JavaScript" />
-          <Panel title="ReactJS" />
-        </div>
-
-        <h1>Project examples:</h1>
-        <div className="flex-time">
-          <Panel
-            title="Lorem Ipsum"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            url=""
-          />
-          <Panel
-            title="Lorem Ipsum"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            url=""
-          />
-          <Panel
-            title="Lorem Ipsum"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-            url=""
-          />
-        </div>
+        <h1>Home</h1>
+        <Skills />
       </div>
     );
   }
