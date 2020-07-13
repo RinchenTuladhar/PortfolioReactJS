f2c36fed72e38e07cc359308be46ae853c43a1a3
RinchenTuladhar <rinchentul@gmail.com>
2020-07-13 20:00:16 +0100

    Styled navbar, added font-aweome & LinkedIn link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
 import React from "react";
+import "./../styles/navbar.scss";
 import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Navbar = () => {
   return (
     <header>
-      <NavLink exact to="/" activeClassName="is-active">
-        Home
-      </NavLink>
-      <NavLink to="/create" activeClassName="is-active">
-        Contact Me
-      </NavLink>
+      <div className="page-links">
+        <NavLink exact to="/" activeClassName="is-active">
+          Home
+        </NavLink>
+        <NavLink to="/contact" activeClassName="is-active">
+          Contact Me
+        </NavLink>
+      </div>
+      <div className="social-links">
+        <a href="https://www.linkedin.com/in/rinchen-tuladhar/" target="_blank">
+          <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+        </a>
+      </div>
     </header>
   );
 };
 
-export default Header;
+export default Navbar;
