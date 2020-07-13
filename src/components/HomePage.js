7b89f242ed78f9e01eede0afe4b50953250f23c2
RinchenTuladhar <rinchentul@gmail.com>
2020-07-13 20:53:01 +0100

    Updated home page content + styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,10 +7,13 @@ export default class Home extends React.Component {
       <div className="home-page">
         <div className="first-row">
           <div className="half-section">
-            <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHS3zbfiftL0g/profile-displayphoto-shrink_200_200/0?e=1600300800&v=beta&t=NvtSeoJhbeQ7EVEJ17zOvq9TlJaEvqydlZe42bwCUZI" />
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C4D03AQHS3zbfiftL0g/profile-displayphoto-shrink_200_200/0?e=1600300800&v=beta&t=NvtSeoJhbeQ7EVEJ17zOvq9TlJaEvqydlZe42bwCUZI"
+              className="center"
+            />
           </div>
           <div className="half-section">
-            <h1>Hello, I'm Rinchen.</h1>
+            <h1>Hello, I'm Rinchen</h1>
             <p>Experienced in Full-Stack Web Development.</p>
             <br />
             <ul>
@@ -27,6 +30,56 @@ export default class Home extends React.Component {
                 </p>
               </li>
             </ul>
+          </div>
+        </div>
+        <div className="second-row">
+          <div className="half-section">
+            <h1>About Me</h1>
+            <p>
+              Hello I'm Rinchen. I am based in Liverpool and have 2 years
+              experience in Web Development.
+            </p>
+            <br />
+            <ul className="tags">
+              <li>
+                <a href="#" className="tag">
+                  HTML
+                </a>
+              </li>
+              <li>
+                <a href="#" className="tag">
+                  CSS
+                </a>
+              </li>
+              <li>
+                <a href="#" className="tag">
+                  SASS
+                </a>
+              </li>
+              <li>
+                <a href="#" className="tag">
+                  JavaScript
+                </a>
+              </li>
+              <li>
+                <a href="#" className="tag">
+                  EmberJS
+                </a>
+              </li>
+              <li>
+                <a href="#" className="tag">
+                  Ruby on Rails
+                </a>
+              </li>
+              <li>
+                <a href="#" className="tag">
+                  PHP
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="half-section">
+            <img src={process.env.PUBLIC_URL + "/images/macbook.jpg"} />
           </div>
         </div>
       </div>
